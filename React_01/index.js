1920a4557041f966a57fafce97454f8247b093de
rajmani-at-362288366897 <rprasad@okkular.io>
2023-01-11 17:52:44 +0530

    Assignment_03

--- React_01/index.js
+++ React_01/index.js
@@ -1,20 +1,51 @@
 import React from "react";
-import  ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
-
-const heading = React.createElement('h2', {}, 'Hello World');
-const para = React.createElement('h3',{}, 'My first React Program');
-const container = React.createElement('div', {className: 'wrapper'}, [heading, para])
+const heading_1 = React.createElement("h1", { key: "11" }, "Hello World");
+const heading_2 = React.createElement("h2", { key: "12" }, "I am Rajmani");
+const heading_3 = React.createElement(
+  "h3",
+  { key: "13" },
+  "Currently working as React Dev. at KeepSpace"
+);
+const container = React.createElement("div", { className: "title" }, [
+  heading_1,
+  heading_2,
+  heading_3,
+]);
 
 const root = document.getElementById("root");
 console.log(container);
 
+const HeaderComponent = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="top_wrapper">
+          <div className="search_logo_wrapper">
+            <div className="logo">
+              <h1>PICAZU</h1>
+            </div>
+
+            <div className="searchbar">
+              <input type="text" /> <button>GO</button>
+            </div>
+          </div>
+
+          <div className="profile">
+            <a href="#">Log in</a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 /**
- * 
+ *
  * React 18, define root
- * 
+ *
  **/
 
- const app_root = ReactDOM.createRoot(root);
- app_root.render(container)
+const app_root = ReactDOM.createRoot(root);
+app_root.render(<HeaderComponent />);
